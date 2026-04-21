463cc6caaff1bfe6c24100ad6c2d0c9ac14af361
Art <art@strangelogic.ltd>
2026-04-21 03:31:04 +0300

    Add Home featured work section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { Hero } from '@/components/hero';
 import { AboutSnippet } from '@/components/about-snippet';
+import { FeaturedWork } from '@/components/featured-work';
 
 export default function Home() {
   return (
     <>
       <Hero />
       <AboutSnippet />
+      <FeaturedWork />
     </>
   );
 }
